be39d4a5d33285b54f7b78bb906ff74d141fa716
Ciobanoiu Theodor <theodor.ciobanoiu@gmail.com>
2022-05-05 22:47:26 +0300

    Spring MVC - Section 13 done

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,10 +3,12 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
     @RequestMapping("/showForm")
     public String showForm(){
@@ -24,6 +26,17 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo! " + theName;
+
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        theName = theName.toUpperCase();
+
+        String result = "Yo(v3)!  " + theName;
 
         model.addAttribute("message", result);
         return "helloworld";
